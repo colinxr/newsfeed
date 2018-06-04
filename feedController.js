b116f76116c570a31986004736ac2eb42f392bd4
colinxr <colinxr@gmail.com>
2018-06-04 16:44:46 -0400

    Enhancement: refactor feed controller

--- feedController.js
+++ feedController.js
@@ -15,9 +15,9 @@ getCategories = (req, res) => {
   res.send(feedCats);
 }
 
-analyzeContent = (item) => {
+analyzeContent = (item, feed) => {
   const title = item.title;
-  const desc = item.summary.replace(/(<([^>]+)>)/ig,"");
+  const desc = item.summary ? item.summary.replace(/(<([^>]+)>)/ig,"") : '';
 
   const text = `${title}. ${desc}`;
   const document = {
@@ -35,8 +35,8 @@ analyzeContent = (item) => {
       const entities = results[0].entities;
       const relevant = entities.filter(relevantEntity);
       const topics = relevant.map(topic => topic.name); // only use names of the entities
-      item.newsMeta.entities = topics;
-
+			item.newsMeta.source = feed;
+			item.newsMeta.entities = topics;
       return item;
     })
    .catch(err => res.status(500).send(err.message));
@@ -54,18 +54,21 @@ filterByDate = (item) => {
 
 parseFeed = (feed) => {
   const httpConfig = {
-    uri: feed,
+    uri: feed.url,
     gzip: true,
   }
   const fpConfig = {
-    feedurl: feed,
+    feedurl: feed.url,
     addmeta: true,
     normalize: true
   }
 
   return feedParser.parse(httpConfig, fpConfig)
     .then(items => {
-      const articles = items.filter(item => filterByDate(item)); // filter items by last 24 hours.
+
+      let articles = items.filter(item => filterByDate(item)) // filter items by last 24 hours.
+			articles = feed.priority > 3 ? articles.slice(0, articles.length / 3) : articles;
+			articles = articles.map(article => analyzeContent(article, feed));
       return articles;
     })
     .catch(err => console.log('Error: ' + err));
@@ -73,14 +76,35 @@ parseFeed = (feed) => {
 
 adminFeed = (req, res) => {
   // take feeds object and pull out all of the individual properties
-  const feedList = Object
+  let feedList = Object
     .keys(feeds)
-    .map(key => Object.keys(feeds[key]).map(k => feeds[key][k]));
+    .map(key => feeds[key])
 
-	const feedUrls = [].concat.apply([], feedList);
+	 feedList = [].concat.apply([], feedList);
 
-  // parse the indivdual urls and hold them in this variable
-  const promises = feedUrls.map(feed => parseFeed(feed));
+	 transformFeedData(res, feedList);
+}
+
+categoryFeed = (req, res) => {
+  // get the category
+  const cat = req.params.category;
+  let feedList = Object
+    .keys(feeds[cat])
+    .map(key => feeds[cat][key]);
+
+	transformFeedData(res, feedList);
+}
+
+singleFeed = (req, res) => {
+  const { category, id } = req.params;
+	const singleFeedObj = feeds[category].filter(feed => feed.name === id);
+
+  transformFeedData(res, singleFeedObj);
+}
+
+transformFeedData = (res, feedList) => {
+	// parse the indivdual urls and hold them in this variable
+  const promises = feedList.map(feed => parseFeed(feed));
 
   // once all promises return values, flatten them in one array, sort it then send off to front-end
   Bluebird.all(promises)
@@ -89,7 +113,6 @@ adminFeed = (req, res) => {
       return stories;
     })
     .then(stories => {
-      // stories = stories.map(story => analyzeContent(story));
       Bluebird.all(stories)
         .then(resp => {
           const date = [`pubdate`];
@@ -100,60 +123,18 @@ adminFeed = (req, res) => {
             });
           }
           const sortedFeed = reverseChron(resp, date);
-          // const last = sortedFeed.length - 1;
-          // console.dir(sortedFeed[last], {depth: null, colors: true});
+		          // const last = sortedFeed.length - 1;
+		          // console.dir(sortedFeed[last], {depth: null, colors: true});
+							// let count = 0;
+							// sortedFeed.map(i => {
+							// 	if (i.newsMeta.source.name == 'cbc-news-world') count++;
+							// });
+							// console.log(count);
           res.send(sortedFeed);
         })
         .catch(err => res.status(500).send(err.message));
     })
     .catch(err => res.status(500).send(err.message));
-}
-
-categoryFeed = (req, res) => {
-  // get the category
-  const cat = req.params.category;
-  // get the urls from that particular category in Feeds Object
-  const feedUrls = Object
-    .keys(feeds[cat])
-    .map(key => feeds[cat][key]);
-  // parses the indivdual urls and holds them in this variable
-  const promises = feedUrls.map(feed => parseFeed(feed));
-  // once all promises return values, flatten them in one array, sort it then send off to front-end
-  Bluebird.all(promises)
-    .then(data => {
-      stories = [].concat(...data) // flatten resp into on array of objects
-      return stories;
-    })
-    .then(stories => {
-      stories = stories.map(story => analyzeContent(story));
-
-      Bluebird.all(stories)
-        .then(data => {
-          const date = [`pubdate`];
-          // sort stories by reverse chron
-          reverseChron = (arr, date) => {
-            return arr.slice().sort((a, b) => {
-              // console.log(a['meta']['title']);
-              return a[date] < b[date] ? 1 : -1;
-            });
-          }
-          const sortedFeed = reverseChron(data, date);
-          // const last = sortedFeed.length - 1;
-          // console.dir(sortedFeed[last], {depth: null, colors: true});
-          res.send(sortedFeed);
-        })
-        .catch(err => res.status(500).send(err.message));
-    })
-    .catch(err => res.status(500).send(err.message));
-}
-
-singleFeed = (req, res) => {
-  const { cat, id } = req.params;
-  // set the feed url
-  const feedUrl = feeds.toronto[id];
-  // parse feed then send results
-  parseFeed(feedUrl)
-    .then(resp => res.send(resp));
 }
 
 module.exports = {
--- feeds.js
+++ feeds.js
@@ -1,30 +1,98 @@
 const feeds = {
-  canada: {
-    'toronto-star-can':    'http://www.thestar.com/content/thestar/feed.RSSManagerServlet.articles.news.canada.rss',
-    'cbc-canada': 'http://rss.cbc.ca/lineup/canada.xml',
-    'cbc-politics': 'http://rss.cbc.ca/lineup/politics.xml',
-    'vice-news-ca': 'https://news.vice.com/en_ca/rss',
-    'global-canada': 'https://globalnews.ca/canada/feed/',
-    },
-  toronto: {
-    'toronto-star-ont': 'http://www.thestar.com/content/thestar/feed.RSSManagerServlet.articles.news.queenspark.rss',
-    'toronto-star-spurr': 'https://www.thestar.com/content/thestar.RSSManagerServlet.articles.authors_spurr_ben.rss',
-    'toronto-star-kalinowski': 'https://www.thestar.com/content/thestar.RSSManagerServlet.articles.authors_kalinowski_tess.rss',
-    'cbc-toronto': 'http://rss.cbc.ca/lineup/canada-toronto.xml',
-    'blog-to': 'http://feeds.feedburner.com/blogto/',
-    'torontoist': 'https://torontoist.com/feed/',
-    'global-toronto': 'https://globalnews.ca/toronto/feed/',
-  },
-  usa: {
+  'canada': [
+    {
+			'name': 'toronto-star-can',
+			'url': 'http://www.thestar.com/content/thestar/feed.RSSManagerServlet.articles.news.canada.rss',
+			'priority': 1,
+		},
+  	{
+			'name': 'cbc-canada',
+			'url': 'http://rss.cbc.ca/lineup/canada.xml',
+			'priority': 1,
+		},
+    {
+			'name': 'cbc-politics',
+			'url': 'http://rss.cbc.ca/lineup/politics.xml',
+			'priority': 2,
+		},
+    {
+			'name': 'vice-news-ca',
+			'url': 'https://news.vice.com/en_ca/rss',
+			'priority': 2,
+		},
+    {
+			'name': 'global-canada',
+			'url': 'https://globalnews.ca/canada/feed/',
+			'priority': 5,
+		},
+  ],
+  'toronto': [
+		{
+			'name': 'toronto-star-ont',
+			'url': 'http://www.thestar.com/content/thestar/feed.RSSManagerServlet.articles.news.queenspark.rss',
+			'priority': 1,
+		},
+    {
+			'name': 'toronto-star-spurr',
+			'url': 'https://www.thestar.com/content/thestar.RSSManagerServlet.articles.authors_spurr_ben.rss',
+			'priority': 5,
+		},
+    {
+			'name': 'toronto-star-kalinowski',
+			'url':'https://www.thestar.com/content/thestar.RSSManagerServlet.articles.authors_kalinowski_tess.rss',
+			'priority': 1,
+		},
+    {
+			'name': 'cbc-toronto',
+			'url':'http://rss.cbc.ca/lineup/canada-toronto.xml',
+			'priority':2,
+		},
+    {
+			'name': 'blog-to',
+			'url':'http://feeds.feedburner.com/blogto/',
+			'priority': 4,
+		},
+    {
+			'name': 'torontoist',
+			'url':'https://torontoist.com/feed/',
+			'priority': 3,
+		},
+    {
+			'name': 'global-toronto',
+			'url':'https://globalnews.ca/toronto/feed/',
+			'priority': 5,
+		},
+  ],
+  'usa': [
     // 'buzzfeed-news': 'https://www.buzzfeed.com/usnews.xml',
-    'nyt-politics': 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml',
-    'the-atlantic-us': 'https://www.theatlantic.com/feed/channel/national/',
-    'washington-post': 'http://feeds.washingtonpost.com/rss/politics',
-    'politico': 'https://www.politico.com/rss/politics08.xml'
-  },
-  other: {
-    'cbc-news-world': 'http://rss.cbc.ca/lineup/world.xml'
-  }
+    {
+			'name': 'nyt-politics',
+			'url': 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml',
+			'priority': 1,
+		},
+    {
+			'name': 'the-atlantic-us',
+			'url': 'https://www.theatlantic.com/feed/channel/national/',
+			'priority': 1,
+		},
+    {
+			'name': 'washington-post',
+			'url': 'http://feeds.washingtonpost.com/rss/politics',
+			'priority': 1,
+		},
+    {
+			'name': 'politico',
+			'url': 'https://www.politico.com/rss/politics08.xml',
+			'priority': 1,
+		},
+  ],
+  'other': [
+    {
+			'name': 'cbc-news-world',
+			'url': 'http://rss.cbc.ca/lineup/world.xml',
+			'priority': 5,
+		},
+  ],
 }
 
 module.exports = { feeds };
